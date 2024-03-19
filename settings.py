84c10d9fe8b7cc414a65ee4f44af66b3ffe07b36
Sabrina Herzog <154417649+sabher-design@users.noreply.github.com>
2024-03-19 14:34:24 +0100

    TP-testing

--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ SESSION_CONFIGS = [
        #men
        #app_sequence=['app1_p1234_intro_women_men', 'word_encryption_trial', 'app3_p5678_men', 'word_encryption', 'app5_p1112_quest_men']
        #women-testing
-       app_sequence=['app1_p1234_intro_women_men', 'app3_p5678_women', 'app5_p1112_quest_women']
+       #app_sequence=['app1_p1234_intro_women_men', 'app3_p5678_women', 'app5_p1112_quest_women']
        #men-testing
        #app_sequence=['app1_p1234_intro_women_men', 'app3_p5678_men', 'app5_p1112_quest_men']
 
@@ -28,7 +28,7 @@ SESSION_CONFIGS = [
        #women-testing
        #app_sequence=['app1_p1234_intro_TP', 'app3_p5678_TP_women', 'app5_p1112_quest_TP']
        #men-testing
-       #app_sequence=['app1_p1234_intro_TP', 'app3_p5678_TP_men', 'app5_p1112_quest_TP']
+       app_sequence=['app1_p1234_intro_TP', 'app3_p5678_TP_men', 'app5_p1112_quest_TP']
 
      ),
 ]
